4f0f1ed01e55eb30c21ab5090429fb5041b2e826
Jacob Silva <Jake@MWH-3.local>
2026-09-22 17:04:24 -0600

    Added FULL STACK files for staff feature

--- backend/app/main(original).py
+++ backend/app/main(original).py
@@ -3,7 +3,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import items, patients, staff
+from app.routers import items, patients
 
 app = FastAPI(title="Simple Items API")
 
@@ -18,7 +18,6 @@ app.add_middleware(
 
 app.include_router(items.router)
 app.include_router(patients.router)
-app.include_router(staff.router)
 
 
 @app.get("/health")
