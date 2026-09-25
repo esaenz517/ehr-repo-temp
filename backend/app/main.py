9f8e3a884cb283cdc9af87f7815cf486a62f644b
Zachary Wittmann <150861571+Zachary-Wittmann@users.noreply.github.com>
2026-09-24 21:46:29 -0600

    Added roles and permission management APIs

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,17 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import drugs, items, patients, providers, staff, rooms
+from app.routers import (
+    drugs,
+    items,
+    patients,
+    permissions,
+    providers,
+    roles,
+    rooms,
+    staff,
+    users,
+)
 
 app = FastAPI(title="Simple Items API")
 
@@ -22,6 +32,10 @@ app.include_router(providers.router)
 app.include_router(drugs.router)
 app.include_router(staff.router)
 app.include_router(rooms.router)
+
+app.include_router(users.router)
+app.include_router(roles.router)
+app.include_router(permissions.router)
 
 
 @app.get("/health")
